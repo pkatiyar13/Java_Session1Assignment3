7d79e2b8527842e4fe3946758ccb6d5cbb96ac37
pkatiyar13 <katiyar.cool@gmail.com>
2016-05-06 12:24:10 +0530

    Java Session 1 Assignment 3
    Java Session 1 Assignment 3

---
+++ S1_A3/src/Assignment3.java
@@ -0,0 +1,13 @@
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		//example 1, double cannot be stored in integer
+		int b = 10.1;
+		//example 2, string cannot be stored in integer
+		int c = "pankaj";
+	}
+
+}
